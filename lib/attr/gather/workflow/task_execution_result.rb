1a3d60ba951b01fcad9f79af7011fdb83bcafaa7
Ian Ker-Seymer <i.kerseymer@gmail.com>
2019-10-24 16:47:39 -0400

    Wrap task execution result (#2)
    * Wrap execution result
    
    * Wrap execution result
    
    * Spec it out
    
    * Document it
    
    * Stricter docs

---
+++ lib/attr/gather/workflow/task_execution_result.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Attr
+  module Gather
+    module Workflow
+      # A wrapper containing information and results of a task execution
+      #
+      # # @!attribute [r] ran_at
+      #     @return [Time] time which the execution occured
+      #
+      # # @!attribute [r] task
+      #     @return [Attr::Gather::Workflow::Task] task that was run
+      #
+      # # @!attribute [r] result
+      #     @return [Dry::Monad::Task] a monad wrapping the state of the task
+      #
+      # @api public
+      TaskExecutionResult = Struct.new(:task, :result) do
+        attr_reader :ran_at
+
+        def initialize(*)
+          @ran_at = Time.now
+          super
+        end
+
+        # Extracts the result, this is an unsafe operation that blocks the
+        # operation, and returns either the value or an exception.
+        #
+        # @note For more information, check out {https://dry-rb.org/gems/dry-monads/1.0/task/#extracting-result}
+        def value!
+          result.value!
+        end
+      end
+    end
+  end
+end
